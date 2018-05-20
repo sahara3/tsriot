13a0e66c4ceab5af676ebc1fb2ec5b916c6bc45f
Soichiro SAHARA <soichiro.sahara@gmail.com>
2018-05-21 04:43:01 +0900

    Modify rollup config to avoid warning.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,11 @@ import buble from 'rollup-plugin-buble';
 export default {
     input: 'lib/tsriot.js',
     external: ['riot'],
-    globals: {'riot': 'riot'},
-    output: {file: 'lib/index.js', format: 'cjs'},
+    output: {
+        file: 'lib/index.js',
+        format: 'cjs',
+        globals: { 'riot': 'riot' },
+    },
     plugins: [
         buble()
     ]
